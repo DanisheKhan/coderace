0e981cc219c45b08aba1739081f96cd3292afab8
DanisheKhan <khandanishe125@gmail.com>
2026-07-22 15:08:18 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Danish Khan</h1>
+      <p>I am a freelancer. Connect with me if you want to develop a project!</p>
+      <p>Email: danishkhan.jsx@gmail.com</p>
+    </>
+  )
+}
+
+export default App;
